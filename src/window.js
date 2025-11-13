69bbacbece2bbb1a0e17977bf628c1bd3f4632b0
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-11-13 14:46:22 +0530

    add snap file and update electron version

--- src/window.js
+++ src/window.js
@@ -2,7 +2,7 @@ const { BrowserWindow, shell, Notification } = require("electron");
 const path = require("path");
 
 const userAgent =
-  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.131 Safari/537.36";
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
 
 let mainWindow;
 
